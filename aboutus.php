7cf20984d79cd234bf5b22a0063a737a1b394a0e
shubhdeeprajput <rajputshubhdeep@gmail.com>
2020-09-10 13:04:03 +0530

    About us and contact us are modified and synced with biraj's code

--- aboutus.php
+++ aboutus.php
@@ -5,6 +5,7 @@
             <link href="https://fonts.googleapis.com/css?family=Amatic+SC:400,700|Dancing+Script:400,700|Frank+Ruhl+Libre:400,500,700|Gloria+Hallelujah|Liu+Jian+Mao+Cao|Ruge+Boogie|Sacramento|Turret+Road:400,500&display=swap" rel="stylesheet">
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
             <link rel="stylesheet" href="./css/styl.css">
+            <link rel="stylesheet" href="./css/aboutus.css">
             <link href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@300&family=Roboto:wght@300&display=swap" rel="stylesheet">
 
 
@@ -18,11 +19,12 @@
 
 <!-- main -->
 
+
 <!-- info section -->
 
 <div class="row infosec">
     <div class="col-6 infosdata">
-            <p>About Website,XploitFree is an IT security site covering the 
+            <p>About Website, XploitFree is an IT security site covering the 
             latest news, research, cyber threats and malware discoveries Monitor 
             for current cyber threats and protect your network, applications,
              and users round-the-clock. Get real time alerting,
@@ -55,49 +57,59 @@
 
 <!-- vulnerabilities -->
 <div class="vulhead">
-    <div class="container">
+    <div class="vulhead1 container">
         <p>We have successfully reported vulnerabilities in....</p>
     </div>
 </div>
 
 <!-- companies -->
 <div class="row compgr justify-content-center">
-    <div class="col-3 compc">
+    <div class="col-3 compc compgoogle">
         <img class="compimg" src="images/companies/google2.0 1.png" alt="">
     </div>
 </div>
 <div class="companies">
     
     <div class="row compr justify-content-around">
-    <div class="col-3 compc">
+    <div class="col-3 compc compleft">
         <img class="compimg" src="images/companies/microsofts-logo-gets-a-makeover 1.png" alt="">
     </div>
-    <div class="col-2 compc ">
+    <div class="col-2 compc compright">
         <img style="width:70%;padding:12%" class="compimg" src="images/companies/Apple-logo-black-and-white 1.png" alt="">
     </div>
     </div>
     <div class="row compr justify-content-around">
-    <div class="col-3 compc">
+    <div class="col-3 compc compleft">
         <img class="compimg" src="images/companies/post-image-550x370 1.png" alt="">
     </div>
-    <div class="col-3 compc">
+    <div class="col-3 compc compright">
         <img style="padding:0;" class="compimg" src="images/companies/github-mark 1.png" alt="">
     </div>
     </div>
-    <div class="row compr justify-content-around">
-    <div class="col-3 compc">
+    <div class="row compr justify-content-around align-items-center">
+    <div class="col-3 compc compleft">
         <img class="compimg" src="images/companies/metro.png" alt="">
     </div>
-    <div class="col-3 compc">
+    <div class="col-3 compc compright">
         <img class="compimg" src="images/companies/dell.png" alt="">
     </div>
     </div>
 </div>
+
 
 <?php
     include 'shared/footer.php';
 ?>
 
     </body>
-
+    <script src="./js/scroll.js"></script>
+    <script>
+        createObserver(.5, "infosdata");
+        createObserver(.5, "infosimg");
+        createObserver(1, "spartinfo");
+        createObserver(1, "vulhead1");
+        createObserver(1, "compgoogle");
+        createObserver(1, "compleft");
+        createObserver(1, "compright");
+    </script>
 </html>
--- contactus.php
+++ contactus.php
@@ -4,10 +4,11 @@
             <title>Contact us</title>
             <link href="https://fonts.googleapis.com/css?family=Amatic+SC:400,700|Dancing+Script:400,700|Frank+Ruhl+Libre:400,500,700|Gloria+Hallelujah|Liu+Jian+Mao+Cao|Ruge+Boogie|Sacramento|Turret+Road:400,500&display=swap" rel="stylesheet">
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-            <link rel="stylesheet" href="./css/styl.css">
             <link href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@300&family=Roboto:wght@300&display=swap" rel="stylesheet">
             <!-- <link href="./styl.css" rel="stylesheet"> -->
             <link rel="stylesheet" href="./css/styl.css">
+            <link rel="stylesheet" href="./css/contactus.css">
+
 
         </head>
 <body>  
@@ -30,13 +31,13 @@
         <p class="fordata"><span style="padding-right: 2%;"><?php mailsvg(40, 40, "black") ?></span>xploitfreesecurity@gmail.com</p>
     </div>
 </div>
-
+<form action="./back/contactUsApi.php" method="POST">
 <div class="row cform">
     <div class="col-6 cformcol">
         <p class="dropp">Or drop a message below</p>
-        <input class="cforminp" type="text"placeholder="Your Name"/>
-        <input class="cforminp" type="text"placeholder="Email"/>
-        <input class="cforminp" type="text"placeholder="Phone Number"/>
+        <input name="name" class="cforminp" type="text"placeholder="Your Name"/>
+        <input name="email" class="cforminp" type="text"placeholder="Email"/>
+        <input name="phone"class="cforminp" type="text"placeholder="Phone Number"/>
     </div>
     <div class="col-6 cformcolimg">
 
@@ -44,23 +45,23 @@
 </div>
 <div class="row cformr2">
     <div class="col-12" style="height:100%">
-    <input class="cforminp1" type="text"placeholder="Subject"/>
+    <input name="subject" class="cforminp1" type="text"placeholder="Subject"/>
     </div>
-</div>
 </div>
 
 <div class="biginp">
     <div class="row biginpr">
         <div class="col-12 biginpc">
-        <textarea class="txtara" placeholder="message" name="" id="" cols="30" rows="10"></textarea>
+        <textarea name="message" class="txtara" placeholder="message" name="" id="" cols="30" rows="10"></textarea>
         </div>
     </div>
     <div class="row biginprb justify-content-end">
         <div class="c2ccolb col-2 d-flex flex-column justify-content-start align-items-center">
-            <button class="c2csub">Submit</button>
+            <button name="submit" class="c2csub">Submit</button>
         </div>
     </div>
 </div>
+</form>
 
 <?php
     include 'shared/footer.php';
